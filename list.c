2386944c493e56c600ed5963ab51eb9fab70d17a
Replit user <replituser@example.com>
2021-03-27 20:13:54 +0000

    new try

--- list.c
+++ list.c
@@ -37,8 +37,8 @@ List * createList() {
 
 void * firstList(List * list) {
   if(list->head == NULL) return NULL;
-  list->current = list->head;
   return &(list->head);
+  list->current = list->head;
 }
 
 void * nextList(List * list) {
