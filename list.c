1a6777d688f64e60e220a3e3a111bd168405cde5
Replit user <replituser@example.com>
2021-03-28 02:17:29 +0000

    new try

--- list.c
+++ list.c
@@ -133,8 +133,7 @@ void * popCurrent(List * list) {
   const void * aux;
   if(list->current->prev == list->head){
     aux = list->current->data;
-    //list->current->next = list->head;
-    list->current->prev = list->current->next;
+    list->head->next->next = list->current->next;
     free(list->current);
   }
   
